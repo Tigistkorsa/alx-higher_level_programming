9f884caec364a16fc49c0331bf52a6f1e4ac98db
tigist korsa <tg5668k@gmail.com>
2024-01-24 13:05:40 -0800

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
-    temp = dir(hidden_4)
-    for i in range(len(temp)):
-        for j in range(len(temp[i])):
-            if (temp[i][j] == '_' and temp[i][j+1] == '_'):
-                break
-            else:
-                print(temp[i])
-                break
+    file = dir(hidden_4)
+    length = len(file)
+    for i in range(0, length):
+        if file[i][0:2] != "__":
+            print(file[i])
